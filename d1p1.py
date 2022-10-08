ba74e9d9a9e4738458915bc7b89404ce267a0f2a
J Sapp <114257109+superbloodmoon@users.noreply.github.com>
2022-10-08 09:24:35 -0500

    Add files via upload

---
+++ d1p1.py
@@ -0,0 +1,16 @@
+def d1p1():
+    input = open('d1p1_data', 'r')
+    input = input.read()
+    input = input.split()
+    input = [int(i) for i in input]
+
+    val = 0
+    for i in range(0, len(input) - 3):
+        if (input[i + 1] + input[i + 2] + input[i + 3]) > (input[i] + input[i + 1] + input[i + 2]):
+            val += 1
+
+
+    print(val)
+
+
+d1p1()
